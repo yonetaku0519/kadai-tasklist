32ff7e45a6769d25fad8da7bbea5716a3bbd4a7a
yoneyone.takutaku <yonetakuyonetaku@test.jp>
2021-05-12 09:49:50 +0000

    user registration

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,21 @@
+    <header class="mb-4">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+            {{-- トップページへのリンク --}}
+            <a class="navbar-brand" href="/">TaskList</a>
+
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="nav-bar">
+                <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav">
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    {{-- ログインページへのリンク --}}
+                    <li><a href="#">Login</a></li>
+                    </ul>
+
+            </div>
+        </nav>
+    </header>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
